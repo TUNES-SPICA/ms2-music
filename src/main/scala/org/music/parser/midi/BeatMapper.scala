d8c1dd9b833fbc7037bdd3906202631856049c4d
SPiCa <chenran@wjacloud.com>
2024-05-29 17:57:04 +0800

    功能-tick 解析音符节拍

--- src/main/scala/org/music/parser/midi/BeatMapper.scala
+++ src/main/scala/org/music/parser/midi/BeatMapper.scala
@@ -1,12 +1,52 @@
 package org.music.parser.midi
 
+import scala.collection.mutable.ArrayBuffer
+
 object BeatMapper {
 
-  def mapBeat(tick: Int, ppq: Int): List[Int] = {
-    var whole = ppq << 2
+  def mapBeat(tick: Int, ppq: Int): ArrayBuffer[Int] = {
+    val wholeNote_ppq = ppq << 2
+    val wholeNote_size = tick / wholeNote_ppq
 
+    val remainder = tick % wholeNote_ppq
 
-    Nil
+    val notes = recursiveNearestPowersOfTwo(remainder, ArrayBuffer[Int]())
+
+    for (i <- 0 until wholeNote_size) notes += 1
+
+    notes
   }
+
+  def main(args: Array[String]): Unit = {
+    val notes = mapBeat(608, 64)
+
+
+  }
+
+  private def recursiveNearestPowersOfTwo(remainder: Int, notes: ArrayBuffer[Int]): ArrayBuffer[Int] = {
+    val note = nearestPowerOfTwo(remainder)
+    if (remainder == note) {
+      notes += note
+      println("note" + note)
+    }
+    else {
+      notes += note / 2
+      println("note" + note / 2)
+      recursiveNearestPowersOfTwo(note - remainder, notes)
+    }
+    notes
+  }
+
+  private def nearestPowerOfTwo(cap: Int): Int = {
+    var n: Int = cap - 1
+    n |= n >>> 1
+    n |= n >>> 2
+    n |= n >>> 4
+    n |= n >>> 8
+    n |= n >>> 16
+
+    n + 1
+  }
+
 
 }
