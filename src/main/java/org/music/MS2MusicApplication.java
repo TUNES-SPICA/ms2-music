49c5c3c3973e259774eebd045fead94ab972a47d
SPiCa <chenran@wjacloud.com>
2024-07-31 17:07:10 +0800

    功能 - 新增启动引导输出

--- src/main/java/org/music/MS2MusicApplication.java
+++ src/main/java/org/music/MS2MusicApplication.java
@@ -8,5 +8,7 @@ public class MS2MusicApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MS2MusicApplication.class, args);
+
+        System.out.println("http://127.0.0.1:8080/index.html");
     }
 }
